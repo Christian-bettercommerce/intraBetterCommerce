67ef55a75823963477a3aed0706bab5e15602791
Christian Quiroz <christianquiroz@MacBook-Pro-de-Christian.local>
2024-06-12 16:46:26 -0400

    Return vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
-import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'hybrid',
-  adapter: netlify()
+  adapter: vercel()
 });
