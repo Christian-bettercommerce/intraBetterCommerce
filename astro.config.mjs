bafbd1384e4dce528883d6b7431dd6f582ea396d
Christian Quiroz <christianquiroz@MacBook-Pro-de-Christian.local>
2024-10-01 10:38:01 -0300

    Fix vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,18 @@ import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import preact from "@astrojs/preact";
-
 import node from "@astrojs/node";
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: 'hybrid',
-  adapter: node({
-    mode: "standalone"
-  }),
-  server:{
+  adapter: vercel(),
+  server: {
     port: import.meta.env.PORT
   },
   image: {
-    service: passthroughImageService(),
+    service: passthroughImageService()
   }
 });
